0f4020add26aecff6da81be439a2ba7fb34615ae
Ayush Srivastava <ayushsri@Ayushs-MacBook-Air.local>
2023-03-06 03:23:52 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let detections = []; // store detection result
 let videoVisibility = true;
 let detecting = false;
 var msg = new SpeechSynthesisUtterance();
+// speechSynthesis.speak(new SpeechSynthesisUtterance('Hello World'));
 
 // global HTML element
 const toggleVideoEl = document.getElementById("toggleVideoEl");
@@ -20,14 +21,22 @@ function preload() {
   console.log("detector object is loaded");
 }
 
+let front = false;
+document.getElementById("flip-button").onclick = () => {
+  front = !front;
+};
+
+const constraints = {
+  video: { facingMode: front ? "user" : "environment" },
+};
 // The setup() function is called once when the program starts.
 function setup() {
   // create canvas element with 640 width and 480 height in pixel
   createCanvas(640, 480);
   // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
   // The element is separate from the canvas and is displayed by default.
-  video = createCapture(VIDEO);
-  video.size(640, 480);
+  video = createCapture(constraints);
+  video.size(1080, 660);
   console.log("video element is created");
   video.elt.addEventListener("loadeddata", function () {
     // set cursor back to default
